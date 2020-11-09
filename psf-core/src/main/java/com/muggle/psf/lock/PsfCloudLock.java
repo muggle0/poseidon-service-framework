88199b60a15ca5fade9d4834748913bbf43af6bb
muggle <isocket@outlook.com>
2020-11-09 19:59:49 +0800

    锁

---
+++ psf-core/src/main/java/com/muggle/psf/lock/PsfCloudLock.java
@@ -0,0 +1,9 @@
+package com.muggle.psf.lock;
+
+/**
+ * @Description:
+ * @Author: muggle
+ * @Date: 2020/11/9
+ **/
+public class PsfCloudLock {
+}
---
+++ psf-redis-starter/src/main/java/com/muggle/psf/JetcacheConfig.java
@@ -0,0 +1,9 @@
+package com.muggle.psf;
+
+/**
+ * @Description:
+ * @Author: muggle
+ * @Date: 2020/11/9
+ **/
+public class JetcacheConfig {
+}
