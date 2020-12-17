1bae66634c82a0bfb2ea0933561213603ddfe031
muggle <isock@outlook.com>
2020-12-17 20:04:34 +0800

    单点登录 和 zuul

---
+++ psf-api-gateway/help.md
@@ -0,0 +1,5 @@
+## 网关处单点登录
+
+https://cloud.spring.io/spring-cloud-security/reference/html/#_single_sign_on
+
+ @EnableOAuth2Sso或@EnableOAuth2Client
--- psf-api-gateway/src/main/java/com/muggle/psf/PsfApiGatewayApplication.java
+++ psf-api-gateway/src/main/java/com/muggle/psf/PsfApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.muggle.psf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableOAuth2Sso
 public class PsfApiGatewayApplication {
 
     public static void main(String[] args) {
