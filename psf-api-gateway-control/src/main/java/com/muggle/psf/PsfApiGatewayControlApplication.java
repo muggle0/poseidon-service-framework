f701170217e06ce980246190531aa2e5f7bfba34
muggle <isocket@outlook.com>
2023-06-03 13:47:13 +0800

    feat(v1): 网关控制器

---
+++ psf-api-gateway-control/src/main/java/com/muggle/psf/PsfApiGatewayControlApplication.java
@@ -0,0 +1,13 @@
+package com.muggle.psf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsfApiGatewayControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsfApiGatewayControlApplication.class, args);
+    }
+
+}
