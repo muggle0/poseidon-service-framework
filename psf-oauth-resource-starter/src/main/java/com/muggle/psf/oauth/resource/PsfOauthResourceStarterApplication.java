49892c89b91795e96b1327b6249307cc30ffa152
muggle <isocket@outlook.com>
2023-09-12 09:24:40 +0800

    ouath 问题版本升级

---
+++ psf-oauth-resource-starter/src/main/java/com/muggle/psf/oauth/resource/PsfOauthResourceStarterApplication.java
@@ -0,0 +1,13 @@
+package com.muggle.psf.oauth.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsfOauthResourceStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsfOauthResourceStarterApplication.class, args);
+	}
+
+}
