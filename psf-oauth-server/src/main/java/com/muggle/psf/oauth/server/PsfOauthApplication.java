6b71e69fbae2c4b0e651a830ec949e1f1385e9da
muggle <isocket@outlook.com>
2023-09-13 09:14:30 +0800

    ouath 问题版本升级

--- psf-oauth-server/src/main/java/com/muggle/psf/oauth/server/PsfOauthApplication.java
+++ psf-oauth-server/src/main/java/com/muggle/psf/oauth/server/PsfOauthApplication.java
@@ -3,7 +3,6 @@ package com.muggle.psf.oauth.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 /**
  * @author muggle
@@ -13,7 +12,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAuthorizationServer
 public class PsfOauthApplication {
     public static void main(final String[] args) {
         SpringApplication.run(PsfOauthApplication.class, args);
